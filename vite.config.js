92e5a97ffeb403791814826679a60c547a5c9c7d
naiemofficial <mail@naiem.info>
2025-05-12 15:06:44 +0600

    Vite config updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/custom.css',
                 'resources/js/app.js',
             ],
             refresh: true,
